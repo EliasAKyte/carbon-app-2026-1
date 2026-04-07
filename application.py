c90c570e2a84b0ea645f8c67946587f415e28be9
EliasAKyte <elias.kyte2@gmail.com>
2026-04-07 13:37:10 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 from capp import application
 from flask_sqlalchemy import SQLAlchemy
+import os
 
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
-application.config['SQLALCHEMY_BINDS']={'transport': 'sqlite:///transport.db'}
+application.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///user.db')
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(application)
 
 class User(db.Model):
@@ -13,12 +14,10 @@ class User(db.Model):
     transport = db.relationship('Transport', backref='author', lazy=True)
 
 class Transport(db.Model):
-    __bind_key__='transport'
     __tablename__='transport_table'
     id = db.Column(db.Integer, primary_key=True)
     kms = db.Column(db.Float)
     user_id = db.Column(db.Integer, db.ForeignKey('user_table.id'), nullable=False)
-
 
 if __name__ == '__main__':
     application.run(debug=True)
