cb3d7c07025cd1429669c1df88ab6016f9cf54bb
EliasAKyte <elias.kyte2@gmail.com>
2026-04-16 13:08:27 +0200

    Enhance carbon_emissions function with CH4 calculation
    Updated carbon emissions calculation to include CH4.

--- capp/carbon_app/functions.py
+++ capp/carbon_app/functions.py
@@ -1,14 +1,26 @@
-efco2={'Bus':{'Diesel':0.10231,'CNG':0.08,'Petrol':0.10231,'No Fossil Fuel':0},
-  'Car':{'Petrol':0.18592,'Diesel':0.16453,'No Fossil Fuel':0},
-  'Plane':{'Petrol':0.24298},
-  'Ferry':{'Diesel':0.11131, 'CNG':0.1131, 'No Fossil Fuel':0},
-  'Motorbike':{'Petrol':0.09816,'No Fossil Fuel':0},
-  'Scooter':{'No Fossil Fuel':0},
-  'Bicycle':{'No Fossil Fuel':0},
-  'Walk':{'No Fossil Fuel':0},
-  'Train':{'Diesel':0.035,'Electric':0}
-}
+#Emissions factor per transport in kg per passenger km
+#Data from: http://efdb.apps.eea.europa.eu/?source=%7B%22query%22%3A%7B%22match_all%22%3A%7B%7D%7D%2C%22display_type%22%3A%22tabular%22%7D
+efco2 = {'Bus':    {'Diesel': 0.10231, 'CNG': 0.08,   'Petrol': 0.10231, 'No Fossil Fuel': 0},
+         'Car':    {'Petrol': 0.18592, 'Diesel': 0.16453,                 'No Fossil Fuel': 0},
+         'Plane':  {'Petrol': 0.24298},
+         'Ferry':  {'Diesel': 0.11131, 'CNG': 0.1131,                    'No Fossil Fuel': 0},
+         'Motorbike': {'Petrol': 0.09816,                                 'No Fossil Fuel': 0},
+         'Scooter':   {'No Fossil Fuel': 0},
+         'Bicycle':   {'No Fossil Fuel': 0},
+         'Walk':      {'No Fossil Fuel': 0}}
+
+efch4 = {'Bus':    {'Diesel': 2e-5,   'CNG': 2.5e-3, 'Petrol': 2e-5,    'No Fossil Fuel': 0},
+         'Car':    {'Petrol': 3.1e-4, 'Diesel': 3e-6,                    'No Fossil Fuel': 0},
+         'Plane':  {'Petrol': 1.1e-4},
+         'Ferry':  {'Diesel': 3e-5,   'CNG': 3e-5,                       'No Fossil Fuel': 0},
+         'Motorbike': {'Petrol': 2.1e-3,                                  'No Fossil Fuel': 0},
+         'Scooter':   {'No Fossil Fuel': 0},
+         'Bicycle':   {'No Fossil Fuel': 0},
+         'Walk':      {'No Fossil Fuel': 0}}
+
 
 def carbon_emissions(kms, transport, fuel):
-    co2=float(kms)*efco2[transport][fuel]
-    return(co2)
+    co2 = float(kms) * efco2[transport][fuel]
+    ch4 = float(kms) * efch4[transport][fuel]
+    total = co2 + ch4
+    return round(co2, 2), round(ch4, 2), round(total, 2)
